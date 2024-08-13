a4a50691bab5207e5a34f50542a2292501ce1886
Adilet <usenkanov.adilet.kanatbekovich@gmail.com>
2024-08-13 14:02:16 +0600

    documentation for auth routes, login, status

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,18 +3,21 @@ import { AuthService } from './auth.service';
 import { LocalGuard } from './guards/local.guard';
 import { Request } from 'express';
 import { JwtGuard } from './guards/jwt.guard';
+import { ApiOperation } from '@nestjs/swagger';
 
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({summary: "Вход в аккаунт и получение JWT токена"})
   @Post('login')
   @UseGuards(LocalGuard)
   async loginPartner(@Req() req: Request) {
     return req.user;
   }
 
+  @ApiOperation({summary: "Получение информации о токене"})
   @Get('status')
   @UseGuards(JwtGuard)
   async status(@Req() req: Request) {
