18405832b18a1dfc8a19bb90f930070fdbb7aaa7
Adilet <usenkanov.adilet.kanatbekovich@gmail.com>
2024-08-15 15:17:34 +0600

    ИЗÐзмеÐchange api in getAttributeByNameDto

--- src/attributes/dto/get-attribute-byName.dto.ts
+++ src/attributes/dto/get-attribute-byName.dto.ts
@@ -3,7 +3,7 @@ import { IsString } from 'class-validator';
 
 export class GetAttributeByNameDto {
 
-  @ApiProperty({ example: "Цвет", description: "Название атрибута" })
+  @ApiProperty({ example: "Цвет", description: "Название характеристики" })
   @IsString({ message: "Должно быть строкой" })
   readonly name: string;
 }
