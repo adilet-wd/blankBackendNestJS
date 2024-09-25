afe9ea51fc532a341e06cf10dfbb4f2cfbf7bf91
Adilet <usenkanov.adilet.kanatbekovich@gmail.com>
2024-09-25 16:18:22 +0600

    brics state 25.09

--- prisma/migrations/20240924072930_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `group_title` to the `Task` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `task` ADD COLUMN `group_title` VARCHAR(191) NOT NULL;
--- prisma/migrations/20240924075747_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `task` ADD COLUMN `complete_user` VARCHAR(191) NOT NULL DEFAULT '';
